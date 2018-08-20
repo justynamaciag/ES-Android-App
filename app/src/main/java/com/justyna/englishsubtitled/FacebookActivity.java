a63c9d7b96960da43eefbfcdb008766bf38639bf
Paweł Taborowski <paweltab@wp.pl>
2018-08-20 23:13:17 +0200

    Pominięcie logowania dla już zalogowanego przez Facebooka użytkownika.
    Na podstawie [dokumentacji](https://developers.facebook.com/docs/facebook-login/android/).

--- app/src/main/java/com/justyna/englishsubtitled/FacebookActivity.java
+++ app/src/main/java/com/justyna/englishsubtitled/FacebookActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -19,6 +20,16 @@ public class FacebookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+        if(isLoggedIn){
+            Intent intent = new Intent(FacebookActivity.this, LessonsActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            FacebookActivity.this.startActivity(intent);
+        }
+
         setContentView(R.layout.activity_facebook);
         callbackManager = CallbackManager.Factory.create();
         loginButton = findViewById(R.id.login_button);
