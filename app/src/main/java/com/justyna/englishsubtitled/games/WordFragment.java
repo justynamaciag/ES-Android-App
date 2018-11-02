e62c644d0797b250eb04a6f5cfe5fa18f0b8ca88
justynamaciag <justynamaciag96@gmail.com>
2018-11-02 19:37:28 +0100

    Fix in word game - added custom adapter

--- app/src/main/java/com/justyna/englishsubtitled/games/WordFragment.java
+++ app/src/main/java/com/justyna/englishsubtitled/games/WordFragment.java
@@ -7,7 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,13 +15,14 @@ import android.widget.Toast;
 
 import com.justyna.englishsubtitled.R;
 import com.justyna.englishsubtitled.model.Translation;
+import com.justyna.englishsubtitled.utils.ButtonAdapter;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class WordFragment extends Fragment {
+public class WordFragment extends Fragment implements ButtonAdapter.customButtonListener {
 
     Translation currentTranslation;
     List<String> buttons;
@@ -29,7 +30,6 @@ public class WordFragment extends Fragment {
     Random rand;
     int checkedIndex;
     View view;
-
 
     WordFragment.OnDataPass dataPasser;
 
@@ -43,7 +43,7 @@ public class WordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        view =  inflater.inflate(R.layout.fragment_word, container, false);
+        view = inflater.inflate(R.layout.fragment_word, container, false);
         Bundle bundle = getArguments();
         if (bundle != null) {
             currentTranslation = (Translation) bundle.getSerializable("translation");
@@ -73,33 +73,19 @@ public class WordFragment extends Fragment {
 
         Collections.shuffle(buttons);
 
-        ArrayAdapter<String> buttonsAdapter = new ArrayAdapter<>(getContext(), R.layout.buttons, R.id.buttons, buttons);
-        GridView buttonsGrid = view.findViewById(R.id.buttonsGridView);
-        buttonsGrid.setNumColumns(5);
-        buttonsGrid.setAdapter(buttonsAdapter);
+        List<Button> btns = new ArrayList<>();
+        for (String b : buttons) {
+            Button btn = new Button(getContext());
+            btn.setText(b);
+            btns.add(btn);
+            System.out.println(btn.getText());
+        }
 
-        buttonsGrid.setOnTouchListener((v, event) -> {
-            try {
-                int index = buttonsGrid.pointToPosition((int) event.getX(), (int) event.getY());
-                System.out.println(buttons);
-
-                String clicked = buttons.get(index);
-                String correct = Character.toString(currentTranslation.getEngWord().charAt(checkedIndex));
-                if (clicked.equals(correct)) {
-
-
-                    letters.get(checkedIndex).setText(clicked);
-                    if (checkedIndex + 1 == currentTranslation.getEngWord().length()) {
-                        Toast.makeText(view.getContext(), "Great!", Toast.LENGTH_SHORT).show();
-                        passData("1");
-                    }
-                    checkedIndex++;
-                }
-            }catch (IndexOutOfBoundsException e){
-                System.out.println(e);
-            }
-            return true;
-        });
+        GridView gridview = (GridView) view.findViewById(R.id.buttonsLayout);
+        gridview.setNumColumns(5);
+        ButtonAdapter a = new ButtonAdapter(getContext(), btns);
+        a.setCustomButtonListner(WordFragment.this);
+        gridview.setAdapter(a);
 
         for (String b : buttons) {
             TextView letterTV = new TextView(getContext());
@@ -110,20 +96,26 @@ public class WordFragment extends Fragment {
         }
 
     }
-    Thread thread = new Thread(){
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(2000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    };
-
 
     public void passData(String data) {
         dataPasser.onDataPass(data);
+    }
+
+    @Override
+    public void onButtonClickListner(Button b) {
+
+        String clicked = String.valueOf(b.getText());
+        String correct = Character.toString(currentTranslation.getEngWord().charAt(checkedIndex));
+
+        if (clicked.equals(correct)) {
+            letters.get(checkedIndex).setText(clicked);
+            if (checkedIndex + 1 == currentTranslation.getEngWord().length()) {
+                Toast.makeText(view.getContext(), "Great!", Toast.LENGTH_SHORT).show();
+                passData("1");
+            }
+            checkedIndex++;
+            b.setEnabled(false);
+        }
     }
 
     public interface OnDataPass {
